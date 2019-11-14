8237cb3a8154edf4774ebc9d0b967ea782023d8e
Neonalig <codychook@gmail.com>
2019-11-14 08:30:46 +0800

    Update Checking

--- Hiragana.xaml.cs
+++ Hiragana.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Globalization;
+using System.Threading.Tasks;
+using Octokit;
 
 namespace JiBoard {
     public partial class MainWindow {
@@ -10,14 +14,44 @@ namespace JiBoard {
         public HiraganaType viewType;
         public bool viewSayings;
         public bool useInput;
+        public bool needsUpdate;
+        public Language currentLang;
+
+        public static Version currentVersion = new Version(0, 2, 0, 0);
 
         public MainWindow() {
             InitializeComponent();
             Width += 10;
             Height += 5;
             ManualImport();
+#if DEBUG
             DebugAll();
+#endif
+            switch (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.ToLowerInvariant()) {
+                case "en":
+                    ChangeLanguage(JiBoard.Language.English);
+                    DisplayLanguage.SelectedIndex = 1;
+                    break;
+                default:
+                    ChangeLanguage(JiBoard.Language.Japanese);
+                    DisplayLanguage.SelectedIndex = 0;
+                    break;
+
+            }
             ChangeView(HiraganaType.Normal);
+            //Task.Run(CheckForUpdate);
+        }
+
+        public async Task CheckForUpdate() {
+            GitHubClient client = new GitHubClient(new ProductHeaderValue("JiBoard"));
+            IReadOnlyList<Release> releases = await client.Repository.Release.GetAll("starflash-studios", "JiBoard");
+            foreach(Release release in releases) {
+                Debug.WriteLine("Found release: " + release.TagName + " >> " + release.Name);
+            }
+            Version latest = new Version(releases[0]);
+            needsUpdate = currentVersion.IsOlder(latest);
+            Debug.WriteLine("Update required: " + needsUpdate);
+            ChangeLanguage(currentLang);
         }
 
         public void ManualImport() {
@@ -69,6 +103,12 @@ namespace JiBoard {
             Import(HbN);
         }
 
+        public void ChangeInputMethod(bool useInput) {
+            this.useInput = useInput;
+            ClipTextInput.IsEnabled = useInput;
+            ClipTextClear.IsEnabled = useInput;
+        }
+
         public void ChangeView(HiraganaType type, bool saying = false) {
             viewType = type;
             viewSayings = saying;
@@ -76,7 +116,47 @@ namespace JiBoard {
             foreach (KeyValuePair<Hiragana, Button> entry in importedHiragana) {
                 entry.Value.Content = entry.Key.Get(type, saying, out bool m);
                 entry.Value.Opacity = m ? 0.5 : 1;
-                entry.Value.FontSize = saying ? 24 : 36;
+                entry.Value.FontSize = saying ? 16 : 36;
+            }
+        }
+
+        public void ChangeLanguage(Language lang) {
+            currentLang = lang;
+            switch (lang) {
+                case JiBoard.Language.Japanese:
+                    Title = "じーぼーど　»　ひらがな";
+                    TransHeading.Content = "ひらがな";
+                    TransHeading.FontSize = 36;
+                    ViewSayingsCheckbox.Content = "はつおん";
+                    ViewSayingsCheckbox.FontSize = 18;
+                    ((ComboBoxItem)ViewTypeCombo.Items[0]).Content = "ひらがな";
+                    ((ComboBoxItem)ViewTypeCombo.Items[1]).Content = "ちさなひらがな";
+                    ((ComboBoxItem)ViewTypeCombo.Items[2]).Content = "はんだくてん";
+                    ((ComboBoxItem)ViewTypeCombo.Items[3]).Content = "だくてん";
+                    ViewTypeCombo.FontSize = 12;
+                    TransClipboardRegion.Content = "くりぷぼっど";
+                    ClipTextClear.Content = "くりあ";
+                    ClipTextClear.FontSize = 16;
+                    ClipTextCopy.Content = "こぴぃ";
+                    //UpdateButton.Content = needsUpdate ? "いま　すぐ　あっぷでと" : "あっぷでと　の　かくにん";
+                    break;
+                case JiBoard.Language.English:
+                    Title = "JiBoard　»　Hiragana";
+                    TransHeading.Content = "Hiragana";
+                    TransHeading.FontSize = 32;
+                    ViewSayingsCheckbox.Content = "Pronunciation";
+                    ViewSayingsCheckbox.FontSize = 10;
+                    ((ComboBoxItem)ViewTypeCombo.Items[0]).Content = "Normal";
+                    ((ComboBoxItem)ViewTypeCombo.Items[1]).Content = "Small";
+                    ((ComboBoxItem)ViewTypeCombo.Items[2]).Content = "Handakuten";
+                    ((ComboBoxItem)ViewTypeCombo.Items[3]).Content = "Dakuten";
+                    ViewTypeCombo.FontSize = 11;
+                    TransClipboardRegion.Content = "Clipboard";
+                    ClipTextClear.Content = "✗";
+                    ClipTextClear.FontSize = 16;
+                    ClipTextCopy.Content = "Copy";
+                    //UpdateButton.Content = needsUpdate ? "Update Now" : "Check for Updates";
+                    break;
             }
         }
 
@@ -114,9 +194,7 @@ namespace JiBoard {
             }
         }
 
-        void UseInputCheck_Click(object sender, RoutedEventArgs e) {
-            useInput = ((CheckBox)sender).IsChecked ?? false;
-        }
+        void UseInputCheck_Click(object sender, RoutedEventArgs e) => ChangeInputMethod(((CheckBox)sender).IsChecked ?? false);
 
         void ClipTextClear_Click(object sender, RoutedEventArgs e) {
             ClipTextInput.Text = "";
@@ -129,6 +207,80 @@ namespace JiBoard {
             Extensions.Clipboard = content;
             Debug.WriteLine($"\t Copied '{content}' to clipboard buffer.");
         }
+
+        void DisplayLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e) => ChangeLanguage((Language)((ComboBox)sender).SelectedIndex);
+
+        void UpdateButton_Click(object sender, RoutedEventArgs e) {
+            if (needsUpdate) {
+                Process.Start(@"https://github.com/starflash-studios/JiBoard/releases");
+            } else {
+                CheckForUpdate();
+            }
+        }
+    }
+
+    public struct Version {
+        public int major;
+        public int minor;
+        public int security;
+        public int typo;
+
+        public override string ToString() => $"{major}.{minor}.{security}.{typo}";
+
+        public Version(int maj = 0, int min = 0, int sec = 0, int typ = 0) {
+            major = maj;
+            minor = min;
+            security = sec;
+            typo = typ;
+        }
+
+        public Version(string parse) {
+            string[] split = parse.Split("."[0]);
+            major = int.Parse(split[0]);
+            minor = int.Parse(split[1]);
+            security = int.Parse(split[2]);
+            typo = int.Parse(split[3]);
+        }
+
+        public Version(Release release) {
+            string[] split = release.TagName.Split("."[0]);
+            major = int.Parse(split[0]);
+            minor = int.Parse(split[1]);
+            security = int.Parse(split[2]);
+            typo = int.Parse(split[3]);
+        }
+
+        /// <summary>
+        /// Returns false if 'other' is a higher version
+        /// </summary>
+        /// <param name="other"></param>
+        /// <param name="levelOfScrutiny">0: Any value, 1: Security or above, 2: Minor or above, 3: Major or above</param>
+        /// <returns></returns>
+        public bool IsNewer(Version other, int levelOfScrutiny = 0) => !IsOlder(other, levelOfScrutiny);
+
+        /// <summary>
+        /// Returns true if 'other' is a higher version
+        /// </summary>
+        /// <param name="other"></param>
+        /// <param name="levelOfScrutiny">0: Any value, 1: Security or above, 2: Minor or above, 3: Major or above</param>
+        /// <returns></returns>
+        public bool IsOlder(Version other, int levelOfScrutiny = 0) {
+            switch (levelOfScrutiny) {
+                case 1:
+                    return other.major > major || other.minor > minor || other.security > security;
+                case 2:
+                    return other.major > major || other.minor > minor;
+                case 3:
+                    return other.major > major;
+                default:
+                    return other.major > major || other.minor > minor || other.security > security || other.typo > typo;
+            }
+        }
+    }
+
+    public enum Language {
+        Japanese,
+        English
     }
 
     public enum HiraganaType {
